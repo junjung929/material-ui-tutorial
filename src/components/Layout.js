94ab04635501a73ed8ee15f2c587ca1e7ca76025
Jun Jung <juny929@gmail.com>
2021-10-22 17:26:40 +0300

    Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,21 @@
-import { makeStyles } from '@material-ui/core';
+import { Drawer, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles({
+  root: {
+    display: 'flex'
+  },
   page: {
     background: '#f9f9f9',
     width: '100%'
+  },
+  drawer: {
+    width: drawerWidth
+  },
+  drawerPaper: {
+    width: drawerWidth
   }
 });
 
@@ -12,11 +23,20 @@ export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
       {/* app bar */}
 
       {/* side drawer */}
-
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        anchor='left'
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant='h5'>Jun's Notes</Typography>
+        </div>
+      </Drawer>
       <div className={classes.page}>
         {children}
       </div>
